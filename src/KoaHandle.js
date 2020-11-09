68cccf314e8218f732fa334678af68b7846f676a
mhio <37205532+mhio@users.noreply.github.com>
2020-11-10 10:23:20 +1100

    include generic handler

--- src/KoaHandle.js
+++ src/KoaHandle.js
@@ -1,27 +1,20 @@
 const fs = require('fs')
 const path = require('path')
-const debug = require('debug')('mh:KoaHandle')
-const base62 = require('base62-random')
-const Promise = require('bluebird')
-const { Exception } = require('@mhio/exception')
-
 const cons = require('consolidate')
+const { KoaGenericHandle } = require('@mhio/koa-generic-handle')
 
-class KoaHandleException extends Exception {}
+const debug = require('debug')('mh:KoaWebHandle')
 
-class KoaHandle {
+class KoaWebHandle extends KoaGenericHandle {
   
   static _initialiseClass(){
-    this.powers = [ 'Bananas', 'Electrons', 'Lemons', 'DeveloperTears' ]
-    const random_power_i = Math.floor(Math.random() * this.powers.length)
-    this.power = this.powers[random_power_i]
     this.views_path = null
     this.views_engine = null
     this.views_extension = null
   }
 
   /**
-   * Setup view engine and path. Only a single 
+   * Setup view engine and path. Effect any instances of KoaHandle as we don't have a constructor yet
    */
   static views({ engine, path: views_path, extension }){
     if (extension) {
@@ -54,21 +47,27 @@ class KoaHandle {
    * @param {object} template     - handlebars template 
    */
   static responseTemplate( object, method, template, engine_override ){
+    // Do we add an extension?
     const template_with_ext = (this.views_extension)
       ? `${template}.${this.views_extension}`
       : template
+    // Do we lookup a path?
     const template_path = (this.views_path)
       ? path.join(this.views_path, template_with_ext)
       : template_with_ext
+    // Do we have an file on disk?
     const template_file_exists = pathExists(template_path)
     if (!template || !template_file_exists) {
       if (!this.views_path) throw new Error(`No views path has been set on KoaHandle to find [${template}]`)
       throw new Error(`Couldn't find template [${template}] in [${this.views_path}]`)
     }
+    // What template engine are we using for this view?
     if (!engine_override && !this.views_engine) {
       throw new Error('No views engine has been set on KoaHandle')
     }
     const engine = engine_override || this.views_engine
+
+    // Now we can create the actual handler function
     return async function(ctx,next){
       const variables = await object[method](ctx, next)
       debug('responseTemplate about to render vars', variables, template_path, engine)
@@ -103,98 +102,8 @@ class KoaHandle {
     }
   }
 
-  /**
-   * @summary  Middleware (early) to start tracking request times and ids
-   * @returns Promise<undefined>
-   */
-  static tracking(){
-    const power = this.power
-    return Promise.coroutine(function* tracking( ctx, next ){
-      const start = Date.now()
-      
-      let request_id = base62(18)
-      ctx.set('x-request-id', request_id)
-      
-      if ( ctx.get('x-transaction-id') === '' ){
-        ctx.set('x-transaction-id', request_id)
-      } else {
-        debug('tracking transaction id attached "%s"', ctx.get('x-transaction-id'))
-      }
-      ctx.set('x-powered-by', power)
-      debug('tracking request', request_id, ctx.ip, ctx.method, ctx.url)
-      
-      yield next()
-      
-      const ms = Date.now() - start
-      ctx.set('x-response-time', `${ms}ms`)
-      debug('tracking response', ctx.get('x-request-id'), ms, ctx.url)
-    })
-  }
-
-  /**
-   * 
-   * @param {object} options 
-   * @param {object} options.logger         - Logger instance following pino API
-   * @return {object}                       - Pinoish logger object
-   */
-  static setOptionsLogger(options){
-    if ( options.logger === false ) return { error: function(){} }
-    if ( options.logger ) return options.logger
-    return console
-  }
-
-  /**
-   * @summary  Error middleware
-   * @param {object} options                - Options
-   * @param {object} options.logger         - Logger instance following pino API
-   * @param {object} options.template       - Template to use in production (recieves `message`, `error`, `ctx`)
-   * @param {object} options.template_dev   - Dev template to use in !production (recieves `message`, `error`, `ctx`)
-   */
-  static error(options = {}){
-    const loggerObj = this.setOptionsLogger(options)
-    const error_template = options.error_template
-    const error_template_development = options.error_template_development 
-    return Promise.coroutine(function* error(ctx, next){
-      try {
-        yield next()
-      } catch (error) {
-        debug('KoaHandle request caught error', error.status, error.message, error)
-        if (!error.status) error.status = 500
-        if (!error.label)  error.label = 'Request Error'
-        // if (!error.simple) error.simple = 'Request Error'
-        if (!error.id)     error.id = base62(12)
-        loggerObj.error('KoaHandle caught error', error)
-        let message = error.simple || 'The request failed'
-        ctx.status = error.status
-        if (process.env.NODE_ENV === 'production') {
-          ctx.body = error_template && error_template({ message, error, ctx })
-            || `<gahh><h3>Error</h3><p>${message}</p><p>ID: ${error.id}</p></gahh>`
-        } else {
-          ctx.body = error_template_development && error_template_development({ message, error, ctx })
-            || `<gahh><h3>Error</h3><p>${message}</p><pre>${JSON.stringify(error, undefined, 2)}</pre></gahh>`
-        }
-      }
-    })
-  }
-
-
 }
-KoaHandle._initialiseClass()
-/*
-KoaHandle.tracking = function* tracking( ctx, next ){
-  const start = Date.now()
-  ctx.set('X-Request-Id', base62(16))
-  if ( ctx.get('X-Transaction-Id') === undefined ){
-    ctx.set('X-Transaction-Id', ctx.get('X-Request-Id'))
-  } else {
-    debug('transaction id attached', ctx.get('X-Transaction-Id'))
-  }
-  ctx.set('x-powered-by', 'Lemons')
-  await next()
-  const ms = Date.now() - start
-  ctx.set('x-response-time', `${ms}ms`)
-}
-*/
+KoaWebHandle._initialiseClass()
 
 function pathExists(test_path){
   try {
@@ -208,8 +117,7 @@ function pathExists(test_path){
 }
 
 module.exports = {
-  KoaHandle,
-  KoaHandleException,
+  KoaWebHandle,
   Exception,
   pathExists,
 }
